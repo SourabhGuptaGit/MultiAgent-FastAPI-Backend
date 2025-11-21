1f5097a085fe01f99717d00ba2ba77f8e24df91b
Sourabh Gupta <sourabhmith.9191@gmail.com>
2025-11-21 21:05:16 +0000

    Changes in main.py to hide any port variables, added watch function in railway config json, that works similar to compose watch

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,9 +3,8 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
-APP_PORT = int(os.getenv("FASTPORT")) if os.getenv("FASTPORT", "").isdigit() else "Not Known"
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to your first FastAPI.", "Port": APP_PORT}
+    return {"message": "Welcome to your first FastAPI."}
 
